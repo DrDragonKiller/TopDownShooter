30ffa56d7fa42aaef7c44bae71a3a0b4d1f0ea75
DrDragonKiller <pwesley.12@gmail.com>
2016-09-10 01:52:10 +0200

    start getting into images
    added Game.java as main class
    added dog.jpg as test iamge

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,19 @@
+import javax.swing.*;
 
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Automatisch generierter Methodenstub
+		JFrame jf = new JFrame();
+		JLabel jl = new JLabel(new ImageIcon("./bin/dog.jpg"));
+		JLabel jl2 = new JLabel(new ImageIcon("./bin/dog.jpg"));
+
+		jl2.setSize(200, 200);
+		jf.getContentPane().add(jl2);
+		jf.getContentPane().add(jl);
+
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.pack();
+		jf.setVisible(true);
 
 	}
 
