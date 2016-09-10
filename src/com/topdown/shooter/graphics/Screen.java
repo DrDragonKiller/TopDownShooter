3eaa43eb0f09aa3525d52486c3118ea817c60a65
DrDragonKiller <pwesley.12@gmail.com>
2016-09-11 01:35:44 +0200

    rendering random map & added graphic plans
    map based on 64*64 tiles (by now)
    each tile is 16*16 (by now)

--- src/com/topdown/shooter/graphics/Screen.java
+++ src/com/topdown/shooter/graphics/Screen.java
@@ -1,14 +1,24 @@
 package com.topdown.shooter.graphics;
 
+import java.util.Random;
+
+
 public class Screen {
 
 	private int	 width, height;
 	public int[] pixels;
+	public int[] tiles = new int[64 * 64];
+
+	private Random random = new Random();
 	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
+
+		for (int i = 0; i < 64 * 64; i++) {
+			tiles[i] = random.nextInt(0xffffff);
+		}
 	}
 	
 	public void clear() {
@@ -19,8 +29,15 @@ public class Screen {
 	
 	public void render() {
 		for (int y = 0; y < height; y++) {
+			if (y < 0 || y >= height) {
+				break;
+			}
 			for (int x = 0; x < width; x++) {
-				pixels[x + y * width] = 0xff00ff;
+				if (x < 0 || x >= width) {
+					break;
+				}
+				int tileIndex = (x >> 4) + (y >> 4) * 64; // x >> 4 == x / 16 BitOperator, schnellere Berechnung da auf binärer Ebene
+				pixels[x + y * width] = tiles[tileIndex];
 			}
 			
 		}
