6b7a69a4d1e163922dd6948dcafb83dc92db7969
Sven <pwesley.12@gmail.com>
2016-09-11 11:39:14 +0200

    📌 moveable (wasd) looping map
    added KeyInput tracker as Keyboard.java

--- src/com/topdown/shooter/Game.java
+++ src/com/topdown/shooter/Game.java
@@ -1,7 +1,6 @@
 package com.topdown.shooter;
 
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
@@ -11,41 +10,45 @@ import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 
 import com.topdown.shooter.graphics.Screen;
+import com.topdown.shooter.input.Keyboard;
 
 
 public class Game extends Canvas implements Runnable {
-
+	
 	private static final long serialVersionUID = -3894242138547079386L;
 	public static int		  width			   = 300;
 	public static int		  height		   = width / 16 * 9;
 	public static int		  scale			   = 3;
 	public static String	  title			   = "TopDownShooter";
-
-	private Thread	thread;
-	private JFrame	frame;
-	private boolean	running	= false;
-
-	private Screen screen;
 	
+	private Thread	 thread;
+	private JFrame	 frame;
+	private Keyboard key;
+	private boolean	 running = false;
+	
+	private Screen screen;
+
 	private BufferedImage image	 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[]		  pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
-
+	
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-
-		screen = new Screen(width, height);
 		
+		screen = new Screen(width, height);
 		frame = new JFrame();
+		key = new Keyboard();
+		addKeyListener(key);
+		
 	}
-
+	
 	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-
-
+	
+	
 	public synchronized void stop() {
 		running = false;
 		try {
@@ -53,9 +56,9 @@ public class Game extends Canvas implements Runnable {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-	}
 
+	}
+	
 	public void run() { // called by thread start
 		long lasttime = System.nanoTime();
 		long timer = System.currentTimeMillis();
@@ -63,12 +66,12 @@ public class Game extends Canvas implements Runnable {
 		double delta = 0;
 		int frames = 0;
 		int updates = 0;
-		
+
 		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lasttime) / ns;
 			lasttime = now;
-			
+
 			while (delta >= 1) {
 				update(); // Method calls 60/s
 				updates++;
@@ -76,48 +79,55 @@ public class Game extends Canvas implements Runnable {
 			}
 			render(); // Method calls as often as possible
 			frames++;
-			
+
 			if (System.currentTimeMillis() - timer > 1000) { // activates once a second
 				timer += 1000; // System.currentTimeMillis() increased by 1000 and so timer decreased by 1000 to reset the subtraction to 0
 				System.out.println(updates + " ups, " + frames + " fps");
 				frame.setTitle(title + "  |  " + updates + " ups, " + frames + " fps");
 				updates = 0;
 				frames = 0;
-				
-				
+
+
 			}
 		}
 		stop();
 	}
-	
+
+	int x = 0, y = 0;
+
 	public void update() {
-		
+		key.update();
+		if (key.up) y--;
+		if (key.down) y++;
+		if (key.left) x--;
+		if (key.right) x++;
 	}
-	
+
+
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
 		if (bs == null) {
 			createBufferStrategy(3); // trippleBuffering
 			return;
 		}
-
-		screen.clear();
-		screen.render();
 		
+		screen.clear();
+		screen.render(x, y);
+
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		// all graphics we need, need to get here:
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
-
+		
 		g.dispose(); // render image
 		bs.show(); // show next buffer
 	}
-
-
+	
+	
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
@@ -127,7 +137,7 @@ public class Game extends Canvas implements Runnable {
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
 }
--- src/com/topdown/shooter/graphics/Screen.java
+++ src/com/topdown/shooter/graphics/Screen.java
@@ -1,45 +1,54 @@
 package com.topdown.shooter.graphics;
 
+import java.awt.Color;
 import java.util.Random;
 
 
 public class Screen {
-
+	
+	public final int MAP_Size	   = 32;
+	public final int MAP_Size_MASK = MAP_Size - 1;
+	public final int Pixel_Size	   = 4;			  // 2^PixelSize
+	
+	
 	private int	 width, height;
 	public int[] pixels;
-	public int[] tiles = new int[64 * 64];
-
-	private Random random = new Random();
+	public int[] tiles = new int[MAP_Size * MAP_Size];
 	
+	private Random random = new Random();
+
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
-
-		for (int i = 0; i < 64 * 64; i++) {
+		
+		for (int i = 0; i < MAP_Size * MAP_Size; i++) {
 			tiles[i] = random.nextInt(0xffffff);
 		}
+		tiles[1] = tiles[2] = tiles[MAP_Size + 1] = 0x000000;
 	}
-	
+
 	public void clear() {
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = 0;
 		}
 	}
-	
-	public void render() {
+
+	public void render(int xOffset, int yOffset) {
 		for (int y = 0; y < height; y++) {
-			if (y < 0 || y >= height) {
-				break;
-			}
+			int yy = y + yOffset;
+			// if (yy < 0 || yy >= height) {
+			// break;
+			// }
 			for (int x = 0; x < width; x++) {
-				if (x < 0 || x >= width) {
-					break;
-				}
-				int tileIndex = (x >> 4) + (y >> 4) * 64; // x >> 4 == x / 16 BitOperator, schnellere Berechnung da auf binärer Ebene
+				int xx = x + xOffset;
+				// if (xx < 0 || xx >= width) {
+				// break;
+				// }
+				int tileIndex = ((xx >> Pixel_Size) & MAP_Size_MASK) + ((yy >> Pixel_Size) & MAP_Size_MASK) * MAP_Size; // x >> 4 == x / 16 BitOperator, schnellere Berechnung da auf binärer Ebene
 				pixels[x + y * width] = tiles[tileIndex];
 			}
-			
+
 		}
 	}
 }
